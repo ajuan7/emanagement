e34d9db62944cc5f68d307d260ee3ce8ab7818b5
ajuan7 <angelo.juanico@hotmail.com>
2025-01-12 18:48:54 +1030

    Add Authentication Login Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,20 +25,13 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>Welcome to AHS Employee Management</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
     </main>
   );
 }
